f1c830aa3f8b240e9dfbd57a82e943cd39b85eb7
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 15:44:23 +0200

    Create Record.cs
    Created a Record class to manage records of the clients and contracts
    
    It will be the base class of Client and Contract

---
+++ SEN371_Project/Record.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEN371_Project
+{
+    internal class Record
+    {
+       public string recordID;
+       public DateTime dateCreated;
+       public string description;
+
+      public void UpdateRecord()
+    
+    }
+}
