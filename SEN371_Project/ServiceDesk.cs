43b75fe909feb7e7df30061f5319ce5ad3b794db
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 18:29:11 +0200

    Update ServiceDesk.cs
    properties and a constructor

--- SEN371_Project/ServiceDesk.cs
+++ SEN371_Project/ServiceDesk.cs
@@ -12,6 +12,31 @@ namespace SEN371_Project
         private string communicationChannel;
         private string clientDetails;
 
+        public string[] ServiceRequestList
+    {
+        get { return serviceRequestList; }
+        private set { serviceRequestList = value; } 
+    }
+
+   
+    public string CommunicationChannel
+    {
+        get { return communicationChannel; }
+        private set { communicationChannel = value; } 
+    }
+
+    
+    public string ClientDetails
+    {
+        get { return clientDetails; }
+        private set { clientDetails = value; } 
+    }
+
+         public ServiceDesk(string[] serviceRequestList, string communicationChannel, string clientDetails)
+    {
+        
+    }
+
         public void logServiceZRequest() { }
         
         private void assignTechnician() { }
