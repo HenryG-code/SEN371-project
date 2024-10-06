6f9a07ec8635c28f43956ce896c6a30b7a316e8f
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 18:38:28 +0200

    Update Record.cs
    Properties

--- SEN371_Project/Record.cs
+++ SEN371_Project/Record.cs
@@ -11,6 +11,27 @@ namespace SEN371_Project
        public string recordID;
        public DateTime dateCreated;
        public string description;
+        
+
+        public string RecordID
+    {
+        get { return recordID; }
+        private set { recordID = value; } 
+    }
+
+    
+    public DateTime DateCreated
+    {
+        get { return dateCreated; }
+        private set { dateCreated = value; } 
+    }
+
+   
+    public string Description
+    {
+        get { return description; }
+        private set { description = value; } 
+    }
 
         public Record(string recordID, DateTime dateCreated, string description)
         {
