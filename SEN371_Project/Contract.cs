0c59f09364ffc3625cde631aab50589f5dbe1ce2
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 16:03:08 +0200

    Update Contract.cs
    Contract inhertied from base class Record.
    
    I also created a constructor.

--- SEN371_Project/Contract.cs
+++ SEN371_Project/Contract.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace SEN371_Project
 {
-    internal class Contract
+    internal class Contract:Record
     {
         private string contractID;
         private string serviceLevel;
         private DateTime renewalDate;
         private int serviceUsage;
+
+        public Contract(string recordId, DateTime dateCreated, string description, string contractId, sting serviceLevel, DateTime renewalDate, int serviceUsage)
+        : base(recordId, dateCreated, description)
+        {
+
+        }
 
         public void viewContractDetails() { }
 
