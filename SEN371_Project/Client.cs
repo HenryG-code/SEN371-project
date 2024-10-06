eefbbedd73ea6785cc8ee366e143e40ea8d685af
HenryG-code <gagianohenry@gmail.com>
2024-10-06 16:16:18 +0200

    2nd commit

--- SEN371_Project/Client.cs
+++ SEN371_Project/Client.cs
@@ -15,6 +15,8 @@ namespace SEN371_Project
         private string contractStatus;
         private int priority;
         **/
+
+        public bool IsFlagged { get; set; }
         public List<string> ServiceHistory { get; set; }
         public List<Contract> Contracts { get; set; }
 
@@ -29,9 +31,37 @@ namespace SEN371_Project
             ServiceHistory.Add(history);
         }
 
-        public void updateClientRecord() { }
+        public void updateClientRecord() { 
 
-        public void flagClient() { }
+            this.Phone = newPhone;
+            Console.WriteLine($"Client's phone number has been updated to: {newPhone}");
+        }
+
+        public void flagClient() { 
+
+            IsFlagged = true;
+            Console.WriteLine($"Client {Name} has been flagged due to contract issues.");
+        }
+
+         public void AddContract(Contract contract)
+        {
+            Contracts.Add(contract);
+            Console.WriteLine($"Contract {contract.ContractID} added for client {Name}.");
+        }
+
+        public void RemoveContract(Contract contract)
+        {
+            Contracts.Remove(contract);
+            Console.WriteLine($"Contract {contract.ContractID} removed for client {Name}.");
+        }
+
+        public void ViewActiveContracts()
+        {
+            foreach (var contract in Contracts)
+            {
+                Console.WriteLine($"Active Contract: {contract.ContractID} - Status: {contract.Status}");
+            }
+        }
 
 
     }
