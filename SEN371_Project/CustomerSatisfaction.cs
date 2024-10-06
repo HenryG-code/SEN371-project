28a459c15a375487c81a321f15cfbdb54ee5a8c6
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 18:41:08 +0200

    Update CustomerSatisfaction.cs
    Properties and Constructor

--- SEN371_Project/CustomerSatisfaction.cs
+++ SEN371_Project/CustomerSatisfaction.cs
@@ -13,7 +13,40 @@ namespace SEN371_Project
         private float stisfactionScore;
         private string complaintDetials;
 
-        //Just creatring the bodys for now
+
+         public string FeedbackID
+    {
+        get { return feedbackID; }
+        private set { feedbackID = value; } 
+    }
+
+
+    public string ClientsID
+    {
+        get { return clientsID; }
+        private set { clientsID = value; } 
+    }
+
+ 
+    public float SatisfactionScore
+    {
+        get { return satisfactionScore; }
+        private set { satisfactionScore = value; } 
+    }
+
+  
+    public string ComplaintDetails
+    {
+        get { return complaintDetails; }
+        private set { complaintDetails = value; } //
+    }
+
+
+    public Feedback(string feedbackID, string clientsID, float satisfactionScore, string complaintDetails)
+    {
+      
+    }
+
 
         public void sendSurvey()
         {
