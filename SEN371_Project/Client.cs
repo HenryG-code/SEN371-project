fe6882887e5e1f1cf321ff110a72d0dfe3f9c145
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 15:52:13 +0200

    Update Client.cs
    Client Inherited from the base class Record.
    
    Also, I created a constructor, and changed (string name) to (string clientName)

--- SEN371_Project/Client.cs
+++ SEN371_Project/Client.cs
@@ -6,13 +6,19 @@ using System.Threading.Tasks;
 
 namespace SEN371_Project
 {
-    internal class Client
+    internal class Client:Record
     {
         private string clientID;
-        private string name;//think we should change this to clientname
+        private string clientName;//think we should change this to clientname
         private string[] serviceHistory;
         private string contractStatus;
         private int priority;
+
+        public Client(string recordId, DateTime dateCreated, string description, string clientID, string clientName, string serviceHistory, string contractStatus, int priority)
+        : base(recordId, dateCreated, description)
+        {
+
+        }
 
         public void updateClientRecord() { }
 
