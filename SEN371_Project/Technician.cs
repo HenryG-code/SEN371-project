a481bf716577568ecf4637152c155197dc965dfd
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 18:35:07 +0200

    Update Technician.cs
    properties and constructor

--- SEN371_Project/Technician.cs
+++ SEN371_Project/Technician.cs
@@ -12,7 +12,41 @@ namespace SEN371_Project
         private string name;
         private string[] skillSet;
         private bool availability;
+        
 
+        public string TechnicianID
+    {
+        get { return technicianID; }
+        private set { technicianID = value; } 
+    }
+
+
+    public string Name
+    {
+        get { return name; }
+        private set { name = value; } 
+    }
+
+    
+    public string[] SkillSet
+    {
+        get { return skillSet; }
+        private set { skillSet = value; } 
+    }
+
+  
+    public bool Availability
+    {
+        get { return availability; }
+        private set { availability = value; } 
+    }
+
+
+ public Technician(string technicianID, string name, string[] skillSet, bool availability)
+    {
+        
+    }
+        
         public void updateJobStatus() { }
 
         public void recieveJobAssignment() { }
