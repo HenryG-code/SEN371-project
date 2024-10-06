86ecf729839b51df71f6491a2fd7a40fc51d6f3b
YTKhaze <92470133+YTKhaze@users.noreply.github.com>
2024-10-06 18:31:33 +0200

    Update FieldService.cs
    Properties

--- SEN371_Project/FieldService.cs
+++ SEN371_Project/FieldService.cs
@@ -11,6 +11,26 @@ namespace SEN371_Project
         private string jobID;
         private string technicianAssignmnet;
         private string jobStatus;
+        
+
+         public string JobID
+    {
+        get { return jobID; }
+        private set { jobID = value; } 
+
+   
+    public string TechnicianAssignment
+    {
+        get { return technicianAssignment; }
+        private set { technicianAssignment = value; }
+    }
+
+    
+    public string JobStatus
+    {
+        get { return jobStatus; }
+        private set { jobStatus = value; } 
+    }
 
         public FieldService(string recordID, DateTime dateCreated, string description, string jobID,
                              string technicianAssignment, string jobStatus) :base(recordID, dateCreated, description)
