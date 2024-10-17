62dd6cc35b12742c4bc515638645719eda5a844b
HenryG <gagiano18@gmail.com>
2024-10-17 23:24:27 +0200

    tests

---
+++ Apex_Care_Solutions_SEN371/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using Apex_Care_Solutions_SEN371.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+public class HomeControllerTests
+{
+    private readonly HomeController _controller;
+
+    public HomeControllerTests()
+    {
+        var mockLogger = new Mock<ILogger<HomeController>>();
+        _controller = new HomeController((IConfiguration)mockLogger.Object);
+    }
+
+    [Fact]
+    public void Login_ValidCredentials_ShouldRedirectToClientManagement()
+    {
+        // Arrange
+        string username = "validuser";
+        string password = "validpassword";
+
+        // Act
+        var result = _controller.Login(username, password) as RedirectToActionResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("ClientManagement", result.ActionName);
+    }
+}
---
+++ MyProject.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MyProject.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
