63181bbe51b8a6aee1099894bebc0450511ff3b6
HenryG <gagiano18@gmail.com>
2024-10-18 22:42:49 +0200

    Comments and code clean up

--- Apex_Care_Solutions_SEN371/SQL testing.sql
+++ Apex_Care_Solutions_SEN371/SQL testing.sql
@@ -5,17 +5,20 @@ Select *
 From Technicians
 
 Delete 
+From Clients
+
+Delete 
 From Technicians
 
+--Testing purposes
 INSERT INTO Technicians (Name, Specialization, Phone, Email, Username, PasswordHash) VALUES 
-('John Doe', 'Electrical', '123-456-7890', 'john.doe@example.com', 'johndoe', '1234'),
-('Jane Smith', 'Plumbing', '098-765-4321', 'jane.smith@example.com', 'janesmith', '12345'),
-('Alice Johnson', 'HVAC', '555-555-5555', 'alice.johnson@example.com', 'alicej', '12345678');
+('Jane Smith', 'Plumbing', '098-765-4321', 'jane.smith@example.com', 'janesmith', '12345');
 
 -- Insert a technician with a hashed password
 INSERT INTO Technicians (Name, Specialization, Phone, Email, Username, PasswordHash)
 VALUES ('John Doe', 'Electrical', '123-456-7890', 'john.doe@example.com', 'johndoe', '$2a$12$e2Xl4GkucgyLCt9qK/xZC.OpHK5k0uWOGoY6iOgkN3RPsFTZogLVa');
 
+--Updating Technician with specific generated hash code 
 UPDATE Technicians
 SET PasswordHash = '$2a$11$6br.xVR9rYpMVfhAltFR2O8UnqMVQwHrbj51lSyhWzgcbfSTDI4We'
 WHERE Username = 'johndoe';
